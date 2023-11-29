93f235c76c5383cb13b5e4a59eb8ce9bcf9faca3
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-29 12:41:33 +0000

    Add form to template

--- things/views.py
+++ things/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
-    if request.POST:
-        form = ThingForm(request.POST)
-    else:
-        form = ThingForm()
+    # if request.GET:
+    #     form = ThingForm(request.GET)
+    # else:
+    #     form = ThingForm()
+    form = ThingForm()
 
     return render(request, 'home.html', {'form': form})
