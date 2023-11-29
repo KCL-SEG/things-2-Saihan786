3983dffb2b04e7a4f24806f4f9072b99abe0c27d
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-29 11:44:40 +0000

    Add else case for GET request

--- things/views.py
+++ things/views.py
@@ -4,5 +4,7 @@ from .forms import ThingForm
 def home(request):
     if request.POST:
         form = ThingForm(request.POST)
+    else:
+        form = ThingForm()
 
     return render(request, 'home.html', {'form': form})
